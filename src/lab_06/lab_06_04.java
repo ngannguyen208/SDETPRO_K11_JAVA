66ea06dead4bc49e59496ca4103a0d0131a16db3
ngannguyen <nganntb208@gmail.com>
2022-09-26 23:15:53 +0700

    lab_06_04

--- src/lab_06/lab_06_04.java
+++ src/lab_06/lab_06_04.java
@@ -1,12 +1,45 @@
 package lab_06;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class lab_06_04 {
     public static void main(String[] args) {
-        /*
-        * String url = "https://google.com";
-        * Check http OR https; domain name, .com OR .net
-        */
+        System.out.print("Please enter URL: ");
+        Scanner scanner = new Scanner(System.in);
+        String url = scanner.nextLine();
+        checkProtocol(url);
+        checkDomail(url);
+    }
 
+    public static void checkProtocol(String url) {
+        String httpPro = "http";
+        String httpsPro = "https";
+        String[] strValue = url.split("://");
+        if (strValue[0].equals(httpPro)) {
+            System.out.println("Protocol is: " + httpPro);
+        } else if (strValue[0].equals(httpsPro)) {
+            System.out.println("Protocol is: " + httpsPro);
+        } else {
+            System.out.println("Sorry!Can't find Protocol http or https.");
+        }
+    }
 
+    public static void checkDomail(String url) {
+        String[] strValue = url.split("://");
+        String dotCom = ".com";
+        String dotNet = ".net";
+        String nameDomain = url.substring(url.indexOf(strValue[1]), url.indexOf("."));
+        System.out.println("Domain name is: " + nameDomain);
+
+        String dotDomain = url.substring(url.indexOf("."));
+        if (dotDomain.equals(dotCom)) {
+            System.out.println("Domain is: " + dotCom);
+        } else if (dotDomain.equals(dotNet)) {
+            System.out.println("Domain is: " + dotNet);
+        } else {
+            System.out.println("Sorry!Can't find Domain .com or .net");
+        }
     }
 }
